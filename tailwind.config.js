af3b785ca5c1aafe08c99ed635f1bcaf9c5db6c5
stephen <stephenola21@gmail.com>
2025-09-10 23:19:48 +0100

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const primeui = require('tailwindcss-primeui');
+module.exports = {
+    content: ['./src/**/*.{html,ts,scss,css}', './index.html'],
+    plugins: [primeui],
+    theme: {
+        screens: {
+            sm: '576px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px',
+            '2xl': '1920px'
+        }
+    }
+};
