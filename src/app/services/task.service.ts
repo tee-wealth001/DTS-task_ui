76678d97cdba3858981ca8fd4e970e5cf0bc9d4b
stephen <stephenola21@gmail.com>
2025-09-10 21:59:06 +0100

    created service requests

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../model/task.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +13,11 @@ export class TaskService {
   constructor(private http: HttpClient) { }
 
 
+  getAll(): Observable<Task[]> { return this.http.get<Task[]>(`${this.base}/tasks`); }
+  get(id: number) { return this.http.get<Task>(`${this.base}/tasks/${id}`); }
+  create(task: TaskService) { return this.http.post<Task>(`${this.base}/tasks`, task); }
+  update(id: number, task: Partial<TaskService>) { return this.http.put<Task>(`${this.base}/tasks/${id}`, task); }
+  updateStatus(id: number, status: string) { return this.http.patch<Task>(`${this.base}/tasks/${id}/status`, { status }); }
+  delete(id: number) { return this.http.delete(`${this.base}/tasks/${id}`); }
 
 }
