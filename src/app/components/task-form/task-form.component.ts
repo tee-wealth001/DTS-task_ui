7086af1d063e9ec563a566e50d6343161cfd6a54
stephen <stephenola21@gmail.com>
2025-09-11 12:04:53 +0100

    added enums

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -116,7 +116,7 @@ export class TaskFormComponent implements OnInit, OnDestroy {
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
 
-    return new Date(`${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`);
+    return new Date(`${year}-${month}-${day}T${hours}:${minutes}:${seconds}`);
   };
 
 
